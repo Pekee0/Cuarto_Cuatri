e96593f44631c0a8cd53c1f7157ea11740928da5
Pekee0 <gonza.marsala@gmail.com>
2024-09-06 10:30:57 -0300

    guia 2 ej 12

--- tp2 js/script.js
+++ tp2 js/script.js
@@ -132,3 +132,22 @@ const array2 = [1,3,5,7,8]
 
 fusionarSinRepetir(array1,array2)
 
+// 10. Lista de Compras: Crea una lista de compras que incluya productos y sus cantidades.
+
+function Producto (nombre,precio,stockDisponible) {
+    this.nombre = nombre,
+    this.precio = precio,
+    this.stockDisponible = stockDisponible
+}
+
+ListadeCompras = []
+
+function AgregarALaLista(Producto)
+{
+    ListadeCompras.push(Producto)
+}
+manzana = new Producto('Manzana',200,3);
+AgregarALaLista(manzana)
+console.log(ListadeCompras)
+
+
