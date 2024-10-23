92530633aef56a525e935a4337305d4a4cd863ec
Pekee0 <gonza.marsala@gmail.com>
2024-10-23 10:20:20 -0300

    tp terminado

--- Guia-DataBinding ANG/dataBinding/src/app/Components/productos/productos.component.ts
+++ Guia-DataBinding ANG/dataBinding/src/app/Components/productos/productos.component.ts
@@ -17,9 +17,20 @@ export class ProductosComponent {
 
     // agregar event Listener para agregar funcionalidad al boton de agregar producto :D 
 
-    AgregarProducto()
+    AgregarProducto(nombre: string, precio: string, descuento: boolean)
     {
-       
+       const nuevoProd = {
+        id: this.productos.length+1,
+        nombre: nombre,
+        precio:+precio,
+        descuento:descuento
+       }
+
+       this.productos.push(nuevoProd); 
     }
 
+    BorrarProducto(id:number)
+    {
+      this.productos = this.productos.filter(elemento =>elemento.id !== id)
+    }
 }
